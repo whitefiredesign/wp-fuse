fe09ab36aaf5e60d0890ad652eb47eea0a8d7b42
bambattajb <joe@white-fire.co.uk>
2017-06-16 09:36:03 +0100

    Util\Less - Added vars argument

--- util/Less.php
+++ util/Less.php
@@ -10,49 +10,35 @@ namespace Fuse\Util;
  */
 class Less {
     
-    static function compile($file = false, $suffix='compiled', $destination=false, $compress = false, $force = false) {
+    static function compile($file = false, $suffix='compiled', $vars=array(), $destination=false, $compress = false, $force = false) {
 
         if(!$file) {
             return false;
         }
 
-        $cache_dir  = get_template_directory() . '/tmp/less/';
+        $less = new \lessc;
 
-        $options    = array(
-            'cache_dir'=> $cache_dir,
-            'compress' => $compress,
-            'cache_method' => 'serialize'
-        );
-
-
-        if($force) {
-
-            // If force compile is passed
-            try {
-                $parser = new \Less_Parser;
-                $parser->parseFile($file);
-                $css    = $parser->getCss();
-                file_put_contents($destination . '/' . basename($file, '.less') . '.' . $suffix . '.css', $css);
-            } catch(\Exception $e) {
-                print_r($e->getMessage());
-            }
-
-        } else {
-
-            // Get styles from cache
-            try {
-                $files = array($file => get_stylesheet_directory_uri());
-                $vars = array(
-                    'fa-font-path' => '"' . get_relative_theme_uri() . '/fonts/"'
-                );
-                $css_file_name = \Less_Cache::Get($files, $options, $vars);
-                copy($options['cache_dir'] . $css_file_name, $destination . '/' . basename($file, '.less') . '.' . $suffix . '.css');
-
-            } catch (\Exception $e) {
-                print_r($e->getMessage());
-            }
+        if($compress) {
+            $less->setFormatter("compressed");
         }
 
+        if(!$destination) {
+            $destination = dirname($file);
+        }
+
+        if(!empty($vars)) {
+            $less->setVariables($vars);
+        }
+
+        try {
+            if($force) {
+                $less->compileFile($file, $destination . '/' . basename($file, '.less') . '.' . $suffix . '.css');
+            } else {
+                $less->checkedCompile($file, $destination . '/' . basename($file, '.less') . '.' . $suffix . '.css');
+            }
+        } catch (\Exception $e) {
+            echo "fatal error: " . $e->getMessage();
+        }
 
         return false;
         
